7305faa5d2d84f0c4fe530d1046c0508d250e443
Deon Ogbuokiri <deon.ogbuokiri@gmail.com>
2021-04-04 21:13:41 +0200

    more changes

--- inc/js/script.js
+++ inc/js/script.js
@@ -17,10 +17,7 @@ jQuery(document).ready(function ($) {
   });
 
   $(".smooth-goto").click(function () {
-    $("html, body").animate(
-      { scrollTop: $(this.hash).offset().top - 50 },
-      1000
-    );
+    $("html, body").animate({ scrollTop: $(this.hash).offset().top - 50 }, 800);
     return false;
   });
 
@@ -35,3 +32,49 @@ jQuery(document).ready(function ($) {
     }
   });
 });
+
+function updateCountdown(countDownDate) {
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the elements
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      // Remove Countdown Header
+      jQuery(".header__countdown").addClass("hide");
+      jQuery(".header__countdown").removeClass("show");
+
+      // Add Release Header
+      jQuery(".header__release").addClass("show");
+      jQuery(".header__release").removeClass("hide");
+    } else if (distance > 0) {
+      clearInterval(x);
+      // Add Countdown Header
+      jQuery(".header__countdown").addClass("show");
+      jQuery(".header__countdown").removeClass("hide");
+
+      // Remove Release Header
+      jQuery(".header__release").addClass("hide");
+      jQuery(".header__release").removeClass("show");
+    }
+  }, 1000);
+}
